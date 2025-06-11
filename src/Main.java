905f7be086bc78c800e79f425f9e4d5ce4b06194
emiainesantos <emilaine95@icloud.com>
2025-06-11 15:27:25 -0300

    Atribuindo  verificação de filmes gratuitos e pagos, calculo de notas, criando sinopse usando o método text block.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun:Maverick");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true; // Verificar se o filme é gratis ou não
+        double notaDoFilme = 8.1;
+
+        //Média de caculo da média.
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        //Sintaxe do Text block utilizando """  texto  """, o texto fica concatenado
+        sinopse = """
+                 Filme Top Gun
+                 Filme de aventura com galã dos anos 80
+                 Muito bom!
+                 Ano de lançamento
+                """ + anoDeLancamento;
+
+        System.out.println(sinopse);
+
+
+
+
+
+
     }
 }
