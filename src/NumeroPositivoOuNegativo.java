d744cf153b07d403c46e226f2c1b07179e81be00
emiainesantos <emilaine95@icloud.com>
2025-06-12 17:10:31 -0300

    Crie um programa que solicite ao usuário digitar um número. Se o número for positivo, exiba "Número positivo", caso contrário, exiba "Número negativo".

---
+++ src/NumeroPositivoOuNegativo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumeroPositivoOuNegativo {
+    public static void main(String[] args) {
+        Scanner numero = new Scanner(System.in);
+
+        System.out.println("Digite um numero ");
+        int valor = numero.nextInt();
+
+        if (valor >= 0) {
+            System.out.println("Numero Positivo");
+
+        } else {
+            System.out.println("Numero negativo");
+        }
+
+    }
+
+}
